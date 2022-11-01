5e23fba53347238b30f2873c30862ea23ddc4ccd
DREAMSCAPER <charles@dreamscaperia.cn>
2022-11-01 16:03:10 +0800

    grammar fix

--- Install.cmd
+++ Install.cmd
@@ -3,6 +3,6 @@ title=Installing...
 color 0a
 echo Please wait while running...
 powershell -file "%~dp0/files/Install.ps1"
-echo Make sure no red warnings is shown above. Or resolve the warnings then re-install it again.
+echo Make sure no red warnings are shown above. Or resolve the warnings then re-install it again.
 echo Reboot ModusToolbox to take effect.
 pause
