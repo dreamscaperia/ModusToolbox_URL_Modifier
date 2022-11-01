05af4b06a3dcf55706f7da05e5728b14b492d102
DREAMSCAPER <charles@dreamscaperia.cn>
2022-11-01 15:50:33 +0800

    improve path recognition

--- files/Install.ps1
+++ files/Install.ps1
@@ -1,11 +1,11 @@
-$InstPath = Get-ChildItem -Path HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall |%{if($_.GetValue("DisplayName") -like "*ModusToolbox*"){$_.GetValue("InstallLocation")}}|Select-Object -Last 1
-if ( -not $InstPath ){ $InstPath = Get-ChildItem -Path HKCU:\Software\Microsoft\Windows\CurrentVersion\Uninstall |%{if($_.GetValue("DisplayName") -like "*ModusToolbox*"){$_.GetValue("InstallLocation")}}|Select-Object -Last 1 }
-Get-ChildItem -Path $instpath -Filter "tool*"|%{
-    Rename-Item -Path ($_.FullName + "\modus-shell\etc\gitconfig") -NewName "gitconfig.old" 2>&1 | Out-Null
+$InstPath = Get-ChildItem -LiteralPath HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall |%{if($_.GetValue("DisplayName") -like "*ModusToolbox*"){$_.GetValue("InstallLocation")}}|Select-Object -Last 1
+if ( -not $InstPath ){ $InstPath = Get-ChildItem -LiteralPath HKCU:\Software\Microsoft\Windows\CurrentVersion\Uninstall |%{if($_.GetValue("DisplayName") -like "*ModusToolbox*"){$_.GetValue("InstallLocation")}}|Select-Object -Last 1 }
+Get-ChildItem -LiteralPath $instpath -Filter "tool*"|%{
+    Rename-Item -LiteralPath ($_.FullName + "\modus-shell\etc\gitconfig") -NewName "gitconfig.old" 2>&1 | Out-Null
 }
 $cur = Split-Path -Parent $MyInvocation.MyCommand.Definition
-Get-ChildItem -Path $instpath -Filter "tool*"|%{
-    Copy-Item -Path ($cur + "\gitconfig") -Destination ($_.FullName + "\modus-shell\etc\")
+Get-ChildItem -LiteralPath $instpath -Filter "tool*"|%{
+    Copy-Item -LiteralPath ($cur + "\gitconfig") -Destination ($_.FullName + "\modus-shell\etc\")
 }
 [System.Environment]::SetEnvironmentVariable('CyRemoteManifestOverride', 'https://itools.infineon.cn/mtb/manifests/mtb-super-manifest-fv2.xml', 'User')
 if (Test-Path env:CY_GETLIBS_OFFLINE) {
--- files/Uninstall.ps1
+++ files/Uninstall.ps1
@@ -1,12 +1,12 @@
-$InstPath = Get-ChildItem -Path HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall |%{if($_.GetValue("DisplayName") -like "*ModusToolbox*"){$_.GetValue("InstallLocation")}}|Select-Object -Last 1
-if ( -not $InstPath ){ $InstPath = Get-ChildItem -Path HKCU:\Software\Microsoft\Windows\CurrentVersion\Uninstall |%{if($_.GetValue("DisplayName") -like "*ModusToolbox*"){$_.GetValue("InstallLocation")}}|Select-Object -Last 1 }
-Get-ChildItem -Path $instpath -Filter "tool*"|%{
-    if (Test-Path -Path ($_.FullName + "\modus-shell\etc\gitconfig.old")) {
-        Remove-Item -Path ($_.FullName + "\modus-shell\etc\gitconfig") -Force
-        Rename-Item -Path ($_.FullName + "\modus-shell\etc\gitconfig.old") -NewName "gitconfig" -Force
-		} else {
-        Remove-Item -Path ($_.FullName + "\modus-shell\etc\gitconfig") -Force
+$InstPath = Get-ChildItem -LiteralPath HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall |%{if($_.GetValue("DisplayName") -like "*ModusToolbox*"){$_.GetValue("InstallLocation")}}|Select-Object -Last 1
+if ( -not $InstPath ){ $InstPath = Get-ChildItem -LiteralPath HKCU:\Software\Microsoft\Windows\CurrentVersion\Uninstall |%{if($_.GetValue("DisplayName") -like "*ModusToolbox*"){$_.GetValue("InstallLocation")}}|Select-Object -Last 1 }
+Get-ChildItem -LiteralPath $instpath -Filter "tool*"|%{
+    if (Test-Path -LiteralPath ($_.FullName + "\modus-shell\etc\gitconfig")) {
+        Remove-Item -LiteralPath ($_.FullName + "\modus-shell\etc\gitconfig") -Force
+        if (Test-Path -LiteralPath ($_.FullName + "\modus-shell\etc\gitconfig.old")) {
+            Rename-Item -LiteralPath ($_.FullName + "\modus-shell\etc\gitconfig.old") -NewName "gitconfig" -Force
 		}
+    }
 }
 [System.Environment]::SetEnvironmentVariable('CyRemoteManifestOverride', '', 'User')
 if (Test-Path env:CY_GETLIBS_OFFLINE1) {
