80a531768934afcee77de3a82a1ff522b3102418
dreamscaperia <charles@dreamscaperia.cn>
2024-01-30 15:26:48 +0800

    BugFix
    Fix the error in uninstallation.

--- files/Uninstall.ps1
+++ files/Uninstall.ps1
@@ -1,12 +1,11 @@
 # Auther: dreamscaperia @github.com
-# Version: 0.1.7.0
+# Version: 0.1.7.5
 
 $InstPath = Get-ChildItem -LiteralPath HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall |%{if($_.GetValue("DisplayName") -like "*ModusToolbox*"){$_.GetValue("InstallLocation")}}|Select-Object -Last 1
 if ( -not $InstPath ){ $InstPath = Get-ChildItem -LiteralPath HKCU:\Software\Microsoft\Windows\CurrentVersion\Uninstall |%{if($_.GetValue("DisplayName") -like "*ModusToolbox*"){$_.GetValue("InstallLocation")}}|Select-Object -Last 1 }
 Get-ChildItem -LiteralPath $instpath -Filter "tool*"|%{
     if (Test-Path -LiteralPath ($_.FullName + "\modus-shell\etc\gitconfig")) {
         Remove-Item -LiteralPath ($_.FullName + "\modus-shell\etc\gitconfig") -Force
-		}
     }
 }
 if ([System.Environment]::GetEnvironmentVariable('CyRemoteManifestOverride','User') -eq 'https://gitee.com/dreamscaperia/mtb-super-manifest/raw/master/mtb-super-manifest-fv2.xml') {
